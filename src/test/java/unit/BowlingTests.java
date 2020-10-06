456e96315bcf6535b78d8cef2d7b4f687fb66b01
Kaiden Rivett <kaidenrivett@uvic.ca>
2020-10-05 22:39:45 -0700

    Committing outline for Bowling exercises

--- src/test/java/unit/BowlingTests.java
+++ src/test/java/unit/BowlingTests.java
@@ -26,7 +26,7 @@ public class BowlingTests {
     @Test
     public void PlayerKnocksDownOnePin() {
         // Arrange
-        BowlingGame game = new BowlingGame(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+        BowlingGame game = new BowlingGame(new int[]{0,0, 0,0, 0,0, 0,0, 1,0, 0,0, 0,0, 0,0, 0,0, 0,0});
 
         // Act
         int result = game.score();
@@ -37,13 +37,32 @@ public class BowlingTests {
 
     @Test
     public void PlayerRollsASpareAndThenFours() {
-        BowlingGame game = new BowlingGame(new int[]{4, 4, 3, 7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4});
+        BowlingGame game = new BowlingGame(new int[]{4,4, 3,7, 4,4, 4,4, 4,4, 4,4, 4,4, 4,4, 4,4, 4,4});
         int result = game.score();
 
         assertEquals(86, result);
     }
 
+    // Exercise 1
 
+    @Test
+    public void ThirdFrameStrike() {
+        BowlingGame game = new BowlingGame(new int[] {2,1, 4,2, 10,0, 3,2, 5,4, 5,2, 1,8, 5,3, 1,5, 7,1});
+        int result = game.score();
 
+//        assertEquals();
+    }
+
+    // Exercise 2
+
+    @Test
+    public void PerfectGame() {
+        BowlingGame game = new BowlingGame(new int[] {});
+        int result = game.score();
+
+//        assertEquals();
+    }
+
+    // Exercise 3: Apply TDD to implemet alternating strikes and spares.
 
 }
