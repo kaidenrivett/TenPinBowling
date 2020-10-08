b90bebc63d24bae8633cea423e34f0122bc67187
Kaiden <kaidenrivett@uvic.ca>
2020-10-07 22:41:27 -0700

    Committing completed exercises from Part 1 of the Lab exercise

--- src/main/java/app/BowlingGame.java
+++ src/main/java/app/BowlingGame.java
@@ -28,21 +28,39 @@ public class BowlingGame {
         for (int i = 0; i < NUMBER_OF_FRAMES; i++) {
             ArrayList<Integer> frameRolls = new ArrayList<>();
 
-            // if a spare is rolled
-            if ((rolls[rollIndex] + rolls[rollIndex + 1] == 10) && (rolls[rollIndex+1]) != 0) {
-                System.out.println("You rolled a spare!");
-                frameRolls.add(rolls[rollIndex]);
-                frameRolls.add(rolls[rollIndex + 1]);
-                frameRolls.add(rolls[rollIndex + 2]);
-            }
-            // if a strike is rolled
-            else if((rolls[rollIndex] == 10)  && (rolls[rollIndex+1] == 0)){
-                System.out.println("You rolled a strike!");
+            // if an alternating game is played
+//            if((rolls[rollIndex] == 10) || (rolls[rollIndex] + rolls[rollIndex+1] == 10)){
+//                frameRolls.add(rolls[rollIndex]);
+//                frameRolls.add(rolls[rollIndex+2]);
+//                frameRolls.add(rolls[rollIndex+3]);
+////                frameRolls.add(rolls[rollIndex+3]);
+//            }
+
+            // if a perfect game is played
+            if((rolls[rollIndex] == 10)  && (rolls[rollIndex+1] == 0) && (rolls[rollIndex+2] == 10)){
                 frameRolls.add(rolls[rollIndex]);
                 frameRolls.add(rolls[rollIndex+2]);
                 frameRolls.add(rolls[rollIndex+3]);
             }
-            else {
+
+            ////// The next if() and else if() statements will also check for the alternating between strikes and spares
+
+            // if a spare is rolled
+             if ((rolls[rollIndex] + rolls[rollIndex + 1] == 10) && (rolls[rollIndex+1]) != 0) {
+//                System.out.println("You rolled a spare!");
+                frameRolls.add(rolls[rollIndex]);
+                frameRolls.add(rolls[rollIndex + 1]);
+                frameRolls.add(rolls[rollIndex + 2]);
+            }
+            // if a single strike is rolled
+            else if((rolls[rollIndex] == 10)  && (rolls[rollIndex+1] == 0) && (rolls[rollIndex+2] != 10)){
+//                System.out.println("You rolled a strike!");
+                frameRolls.add(rolls[rollIndex] );
+                frameRolls.add(rolls[rollIndex+2]);
+                frameRolls.add(rolls[rollIndex+3]);
+            }
+
+            else{
                 frameRolls.add(rolls[rollIndex]);
                 frameRolls.add(rolls[rollIndex + 1]);
             }
--- src/test/java/unit/BowlingTests.java
+++ src/test/java/unit/BowlingTests.java
@@ -49,20 +49,29 @@ public class BowlingTests {
     public void ThirdFrameStrike() {
         BowlingGame game = new BowlingGame(new int[] {5,2, 1,1, 10,0, 5,3, 2,1, 5,4, 8,1, 4,4, 5,1, 7,2});
         int result = game.score();
-
         assertEquals(79, result);
     }
 
     // Exercise 2
+    // 12 frames are played when you score a perfect game
 
     @Test
     public void PerfectGame() {
-        BowlingGame game = new BowlingGame(new int[] {10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0});
+        BowlingGame game = new BowlingGame(new int[] {10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0, 10,0});
         int result = game.score();
-
+        System.out.println("You scored: "+ result);
         assertEquals(300, result);
     }
 
     // Exercise 3: Apply TDD to implement alternating strikes and spares.
+    @Test
+    public void AlternatingGame() {
+        // alternating game requires one more frame to be played, in our case where the score alternates between spare and strike,
+        // our last frame will be a strike
+        BowlingGame game = new BowlingGame(new int[] {10,0, 7,3, 10,0, 5,5, 10,0, 2,8, 10,0, 6,4, 10,0, 4,6, 10,0});
+        int result = game.score();
+        System.out.println("Your score is " + result);
+        assertEquals(200, result);
+    }
 
 }
